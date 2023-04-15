b35b33c52b87c3614ab95f1a2d2b85d4fcdd74a6
StarklyUnnamed <sshind90192@gmail.com>
2023-04-15 15:18:11 -0500

    Added module-level docString for 'calculations.py'.

--- calculator/calculations.py
+++ calculator/calculations.py
@@ -1,5 +1,30 @@
-from typing import Union
 
+"""Provide several sample math calculations.
+
+This module allows the user to make mathematical calculations.
+
+Examples:
+    >>> from calculator import calculations
+    >>> calculations.add(2, 4)
+    6.0
+    >>> calculations.multiply(2.0, 4.0)
+    8.0
+    >>> from calculator.calculations import divide
+    >>> divide(4.0, 2)
+    2.0
+
+
+The module contains the following functions:
+
+- `add(a, b)` - Returns the sum of two numbers.
+- `subtract(a, b)` - Returns the difference of two numbers.
+- `multiply(a, b)` - Returns the product of two numbers.
+- `divide(a, b)` - Returns the quotient of two numbers.
+"""
+
+from typing import Union
+# Modern python would implement typeHinting the following way:
+# def function(a: float | int, b: float | int) -> float
 def add(a: Union[float, int], b: Union[float, int]) -> float:
     """Compute and return the sum of two values.
 
@@ -10,14 +35,15 @@ def add(a: Union[float, int], b: Union[float, int]) -> float:
         6.0
 
     Args:
-        a (float, int): A number representing the first addend in the addition.
-        b (float, int): A number representing the second addend in the addition.
+        a: A number representing the first addend in the addition.
+        b: A number representing the second addend in the addition.
 
     Returns:
-        float: The sum of 'a' and 'b'
+        The sum of 'a' and 'b'
     """
     return float(a + b)
-
+# Modern python would implement typeHinting the following way:
+# def function(a: float | int, b: float | int) -> float
 def subtract(a: Union[float, int], b: Union[float, int]) -> float:
     """Returns the difference of two numbers.
 
@@ -28,14 +54,15 @@ def subtract(a: Union[float, int], b: Union[float, int]) -> float:
         2.0
 
     Args:
-        a (float, int): A number representing the minuend of the subtraction.
-        b (float, int): A number representing the subtrahend of the subtraction.
+        a: A number representing the minuend of the subtraction.
+        b: A number representing the subtrahend of the subtraction.
 
     Returns:
-        float: A number that is the difference between both arguments.
+        A number that is the difference between both arguments.
     """
     return float(a - b)
-
+# Modern python would implement typeHinting the following way:
+# def function(a: float | int, b: float | int) -> float
 def multiply(a: Union[float, int], b: Union[float, int]) -> float:
     """Returns the product of two numbers.
 
@@ -46,14 +73,15 @@ def multiply(a: Union[float, int], b: Union[float, int]) -> float:
         8.0
 
     Args:
-        a (float, int): A number representing the multiplicand in the multiplication.
-        b (float, int): A number representing the multiplier in the multiplication.
+        a: A number representing the multiplicand in the multiplication.
+        b: A number representing the multiplier in the multiplication.
 
     Returns:
-        float: The product of both arguments.
+        The product of both arguments.
     """
     return float(a * b)
-
+# Modern python would implement typeHinting the following way:
+# def function(a: float | int, b: float | int) -> float
 def divide(a: Union[float, int], b: Union[float, int]) -> float:
     """Returns the quotient of two numbers. 
 
@@ -64,14 +92,14 @@ def divide(a: Union[float, int], b: Union[float, int]) -> float:
         2.0
 
     Args:
-        a (float, int): A number that represents the dividend in the division.
-        b (float, int): A number that represents the divider in the division.
+        a: (float, int): A number that represents the dividend in the division.
+        b: (float, int): A number that represents the divider in the division.
 
     Raises:
         ZeroDivisionError: An error will occur if the divider is 0.
 
     Returns:
-        float: The quotient of both arguments.
+        The quotient of both arguments.
     """
     if b == 0:
         raise ZeroDivisionError("division by zero")
